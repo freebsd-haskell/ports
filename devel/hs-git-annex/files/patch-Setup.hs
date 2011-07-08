41884a76b5d8e797252dc2fca464897c307d2cb6
Gabor Pali <pali.gabor@gmail.com>
2011-07-08 23:35:53 +0200

    devel/hs-git-annex: Add support for manual pages

--- devel/hs-git-annex/files/patch-Setup.hs
+++ devel/hs-git-annex/files/patch-Setup.hs
@@ -1,11 +1,11 @@
---- Setup.hs.old	2011-07-02 23:11:40.000000000 +0200
-+++ Setup.hs	2011-07-06 17:35:42.374288187 +0200
+--- ./Setup.hs.orig	2011-07-02 23:11:40.000000000 +0200
++++ ./Setup.hs	2011-07-08 23:11:28.256484379 +0200
 @@ -9,9 +9,9 @@
  }
  
  makeSources _ _ = do
 -	system "make sources"
-+	system "gmake sources"
++	system "gmake sources manuals"
  	return (Nothing, [])
  
  makeClean _ _ _ _ = do
