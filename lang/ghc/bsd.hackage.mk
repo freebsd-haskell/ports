7bb57868e5f355203b064168a26ff5b6d2fc36dd
Gabor Pali <pali.gabor@gmail.com>
2011-08-02 16:35:05 +0200

    Integrate from the Ports Collection

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -1,5 +1,5 @@
 #
-# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.34 2011/07/10 21:33:00 ashish Exp $
+# $FreeBSD: ports/lang/ghc/bsd.hackage.mk,v 1.36 2011/08/01 13:43:49 pgj Exp $
 #
 # bsd.hackage.mk -- List of Haskell Cabal ports.
 #
