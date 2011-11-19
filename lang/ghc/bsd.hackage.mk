b320d92991999ffdd52a5849cfe96422ff9c03be
Gabor Pali <pali.gabor@gmail.com>
2011-11-19 21:00:31 +0100

    Add security/hs-crypto-pubkey-types

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -58,6 +58,7 @@ cpphs_port=			devel/hs-cpphs		# executable
 criterion_port=			benchmarks/hs-criterion
 Crypto_port=			security/hs-Crypto
 crypto-api_port=		security/hs-crypto-api
+crypto-pubkey-types_port=	security/hs-crypto-pubkey-types
 csv_port=			textproc/hs-csv
 curl_port=			ftp/hs-curl
 darcs_port=			devel/hs-darcs		# executable
