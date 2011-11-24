0e19696d9077656761a411acbe6db4f5e0a7585b
Gabor Pali <pali.gabor@gmail.com>
2011-11-24 10:26:16 +0100

    benchmarks/hs-criterion: Fix build with recent version of math/hs-statistics

---
+++ benchmarks/hs-criterion/files/patch-Criterion__Analysis__Types.hs
@@ -0,0 +1,25 @@
+--- ./Criterion/Analysis/Types.hs.orig	2011-11-17 19:40:31.000000000 +0100
++++ ./Criterion/Analysis/Types.hs	2011-11-24 09:45:43.000000000 +0100
+@@ -145,3 +145,21 @@
+                            v .: "stdDev" <*>
+                            v .: "outliers"
+     parseJSON _ = empty
++
++instance ToJSON B.Estimate where
++    toJSON e =
++     object [ "estPoint"           .= (B.estPoint e)
++            , "estLowerBound"      .= (B.estLowerBound e)
++            , "estUpperBound"      .= (B.estUpperBound e)
++            , "estConfidenceLevel" .= (B.estConfidenceLevel e)
++            ]
++
++instance FromJSON B.Estimate where
++    parseJSON (Object v) =
++      B.Estimate <$>
++        v .: "estPoint" <*>
++        v .: "estLowerBound" <*>
++        v .: "estUpperBound" <*>
++        v .: "estConfidenceLevel"
++
++    parseJSON _ = empty
+\ No newline at end of file
