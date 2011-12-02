43cf62d5511a297cb50b9c92b4a7f332e28bc145
Gabor Pali <pali.gabor@gmail.com>
2011-12-02 16:07:31 +0100

    devel/hs-configurator: 0.1.0.0 --> 0.2.0.0

--- devel/hs-configurator/files/patch-Data__Configurator.hs
+++ devel/hs-configurator/files/patch-Data__Configurator.hs
@@ -1,39 +1,21 @@
---- ./Data/Configurator.hs.orig	2011-09-19 20:23:30.000000000 +0200
-+++ ./Data/Configurator.hs	2011-11-22 13:55:11.000000000 +0100
-@@ -69,9 +69,8 @@
- import Data.Configurator.Types.Internal
+--- ./Data/Configurator.hs.orig	2011-11-21 23:17:06.000000000 +0100
++++ ./Data/Configurator.hs	2011-12-02 16:02:01.000000000 +0100
+@@ -70,7 +70,7 @@
  import Data.IORef (atomicModifyIORef, newIORef, readIORef)
  import Data.Maybe (fromMaybe, isJust)
--import Data.Monoid (mconcat)
+ import Data.Monoid (mconcat)
 -import Data.Text.Lazy.Builder (fromString, fromText, toLazyText)
--import Data.Text.Lazy.Builder.Int (decimal)
-+import Data.Monoid
-+import Data.Text.Lazy.Builder
- import Prelude hiding (catch, lookup)
- import System.Environment (getEnv)
- import System.IO (hPutStrLn, stderr)
-@@ -84,6 +83,24 @@
- import qualified Data.Text as T
++import Data.Text.Lazy.Builder (Builder, fromText, toLazyText)
+ import Data.Text.Lazy.Builder.Int (decimal)
+ import Data.Text.Lazy.Builder.RealFloat (realFloat)
+ import Data.Ratio (denominator, numerator)
+@@ -87,6 +87,9 @@
  import qualified Data.Text.Lazy as L
  import qualified Data.Text.Lazy.IO as L
-+import Data.Char (chr,ord)
-+
-+decimal :: Integral a => a -> Builder
-+decimal i
-+  | i < 0     = singleton '-' `mappend` go (-i)
-+  | otherwise = go i
-+  where
-+    go n | n < 10    = digit n
-+         | otherwise = go (n `quot` 10) `mappend` digit (n `rem` 10)
-+
-+i2d :: Int -> Char
-+i2d i = chr (ord '0' + i)
-+
-+digit :: Integral a => a -> Builder
-+digit n = singleton $! i2d (fromIntegral n)
-+
+ 
 +fromString :: String -> Builder
 +fromString = fromText . T.pack
- 
++
  loadFiles :: [Worth Path] -> IO (H.HashMap (Worth Path) [Directive])
  loadFiles = foldM go H.empty
+  where
