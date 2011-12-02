c94c787833f82f5b7f728f67bade94fede6cc0a0
Gabor Pali <pali.gabor@gmail.com>
2011-12-02 18:10:18 +0100

    Add devel/hs-ConfigFile

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -55,6 +55,7 @@ clientsession_port=		security/hs-clientsession
 cmdargs_port=			devel/hs-cmdargs
 comonad_port=			math/hs-comonad
 comonad-transformers_port=	math/hs-comonad-transformers
+ConfigFile_port=		devel/hs-ConfigFile
 configurator_port=		devel/hs-configurator
 contravariant_port=		math/hs-contravariant
 convertible_port=		devel/hs-convertible
