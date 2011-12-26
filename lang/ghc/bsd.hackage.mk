a4b96748c63aa588931b44d876afee2eaadcf2b9
Gabor Pali <pali.gabor@gmail.com>
2011-12-26 04:49:31 +0100

    Add mail/hs-email-validate

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -100,6 +100,7 @@ distributive_port=		math/hs-distributive
 dlist_port=			devel/hs-dlist
 double-conversion_port=		textproc/hs-double-conversion
 dyre_port=			devel/hs-dyre
+email-validate_port=		mail/hs-email-validate
 enumerator_port=		devel/hs-enumerator
 entropy_port=			security/hs-entropy
 epic_port=			lang/hs-epic		# executable
