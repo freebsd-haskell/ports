84b3ba7f12dc3379fc97988536fb67a4dbec25e2
Gabor Pali <pali.gabor@gmail.com>
2012-01-05 20:37:40 +0100

    www/hs-yesod-auth: Fix build

--- www/hs-yesod-core/files/patch-Yesod__Dispatch.hs
+++ www/hs-yesod-core/files/patch-Yesod__Dispatch.hs
@@ -1,6 +1,6 @@
 --- ./Yesod/Dispatch.hs.orig	2011-12-26 11:59:27.000000000 +0100
-+++ ./Yesod/Dispatch.hs	2012-01-05 17:23:25.000000000 +0100
-@@ -15,8 +15,8 @@
++++ ./Yesod/Dispatch.hs	2012-01-05 19:53:51.000000000 +0100
+@@ -15,8 +15,9 @@
      , mkYesodDispatch
      , mkYesodSubDispatch
        -- ** Path pieces
@@ -8,10 +8,11 @@
 -    , MultiPiece (..)
 +    , PathPiece (..)
 +    , toSinglePiece
++    , fromSinglePiece
      , Texts
        -- * Convert to WAI
      , toWaiApp
-@@ -31,7 +31,7 @@
+@@ -31,7 +32,7 @@
  import Yesod.Internal.Dispatch
  import Yesod.Widget (GWidget)
  
