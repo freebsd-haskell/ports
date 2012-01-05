b388d7eebba365bcb5015d124988bc6d043fc4ed
Gabor Pali <pali.gabor@gmail.com>
2012-01-05 14:15:15 +0100

    www/hs-yesod-core: Fix build

---
+++ www/hs-yesod-core/files/patch-Yesod__Dispatch.hs
@@ -0,0 +1,21 @@
+--- ./Yesod/Dispatch.hs.orig	2011-12-26 11:59:27.000000000 +0100
++++ ./Yesod/Dispatch.hs	2012-01-05 14:12:21.000000000 +0100
+@@ -15,8 +15,7 @@
+     , mkYesodDispatch
+     , mkYesodSubDispatch
+       -- ** Path pieces
+-    , SinglePiece (..)
+-    , MultiPiece (..)
++    , PathPiece (..)
+     , Texts
+       -- * Convert to WAI
+     , toWaiApp
+@@ -31,7 +30,7 @@
+ import Yesod.Internal.Dispatch
+ import Yesod.Widget (GWidget)
+ 
+-import Web.PathPieces (SinglePiece (..), MultiPiece (..))
++import Web.PathPieces
+ import Yesod.Internal.RouteParsing (THResource, Pieces (..), createRoutes, createRender, Resource (..), parseRoutes, parseRoutesNoCheck, parseRoutesFile, parseRoutesFileNoCheck)
+ import Language.Haskell.TH.Syntax
+ 
