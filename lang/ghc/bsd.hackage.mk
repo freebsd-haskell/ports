cd39a7c44c0cf15ca61beb8d6258b395d1c74365
Gabor Pali <pali.gabor@gmail.com>
2012-04-07 08:38:47 +0200

    Add devel/hs-bloomfilter

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -39,6 +39,7 @@ blaze-builder-enumerator_port=	devel/hs-blaze-builder-enumerator
 blaze-html_port=		textproc/hs-blaze-html
 blaze-textual_port=		devel/hs-blaze-textual
 blaze-textual-native_port=	devel/hs-blaze-textual-native
+bloomfilter_port=		devel/hs-bloomfilter
 BNFC_port=			devel/hs-BNFC		# executable
 Boolean_port=			devel/hs-Boolean
 brainfuck_port=			lang/hs-brainfuck	# executable
