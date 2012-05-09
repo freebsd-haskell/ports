3bc20e2fe35d047ad15a832b2c54c10d5461c06c
Gabor Pali <pali.gabor@gmail.com>
2012-05-09 11:32:28 +0200

    devel/hs-darcs: 2.5.2 --> 2.8.0

--- devel/hs-darcs/files/patch-src__Darcs__Repository.hs
+++ devel/hs-darcs/files/patch-src__Darcs__Repository.hs
@@ -1,11 +1,11 @@
---- ./src/Darcs/Repository.hs.orig	2011-03-13 23:40:46.000000000 +0100
-+++ ./src/Darcs/Repository.hs	2012-04-09 13:25:55.000000000 +0200
-@@ -320,7 +320,7 @@
-         renameFile p' p
-       writeCompressed xs
-     _ -> fail "Unexpected non-file tar entry"
--  writeCompressed (Tar.Fail e) = fail e
-+  writeCompressed (Tar.Fail e) = fail $ show e
-   putInfo = when (not $ Quiet `elem` opts) . putStrLn
+--- ./src/Darcs/Repository.hs.orig	2012-04-22 17:14:14.000000000 +0200
++++ ./src/Darcs/Repository.hs	2012-05-09 11:30:29.000000000 +0200
+@@ -314,7 +314,7 @@
  
- -- | writePatchSet is like patchSetToRepository, except that it doesn't
+ unpackTar :: Cache -> IO () -> Tar.Entries -> IO ()
+ unpackTar  _ _ Tar.Done = return ()
+-unpackTar  _ _ (Tar.Fail e)= fail e
++unpackTar  _ _ (Tar.Fail e) = fail $ show e
+ unpackTar c mh (Tar.Next x xs) = case Tar.entryContent x of
+   Tar.NormalFile x' _ -> do
+     let p = Tar.entryPath x
