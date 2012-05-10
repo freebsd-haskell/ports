34a26783fe44a84e41bac5968152c2a770020b43
Gabor Pali <pali.gabor@gmail.com>
2012-05-10 16:57:50 +0200

    hs-darcs: Fix build with tar 0.4

--- devel/hs-darcs/files/patch-src__Darcs__Repository.hs
+++ devel/hs-darcs/files/patch-src__Darcs__Repository.hs
@@ -1,8 +1,23 @@
 --- ./src/Darcs/Repository.hs.orig	2012-04-22 17:14:14.000000000 +0200
-+++ ./src/Darcs/Repository.hs	2012-05-09 11:30:29.000000000 +0200
-@@ -314,7 +314,7 @@
++++ ./src/Darcs/Repository.hs	2012-05-10 16:44:09.000000000 +0200
+@@ -302,19 +302,19 @@
+ removeMetaFiles = mapM_ (removeFile . (darcsdir </>)) .
+   filter ("meta-" `isPrefixOf`) =<< getDirectoryContents darcsdir
  
- unpackTar :: Cache -> IO () -> Tar.Entries -> IO ()
+-unpackBasic :: Cache -> Tar.Entries -> IO ()
++unpackBasic :: Cache -> Tar.Entries Tar.FormatError -> IO ()
+ unpackBasic c x = do
+   withControlMVar $ \mv -> unpackTar c (basicMetaHandler c mv) x
+   removeMetaFiles
+ 
+-unpackPatches :: Cache -> [String] -> Tar.Entries -> IO ()
++unpackPatches :: Cache -> [String] -> Tar.Entries Tar.FormatError -> IO ()
+ unpackPatches c ps x = do
+   withControlMVar $ \mv -> unpackTar c (patchesMetaHandler c ps mv) x
+   removeMetaFiles
+ 
+-unpackTar :: Cache -> IO () -> Tar.Entries -> IO ()
++unpackTar :: Cache -> IO () -> Tar.Entries Tar.FormatError -> IO ()
  unpackTar  _ _ Tar.Done = return ()
 -unpackTar  _ _ (Tar.Fail e)= fail e
 +unpackTar  _ _ (Tar.Fail e) = fail $ show e
