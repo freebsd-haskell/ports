e830be7ba71fe606ea664f81590e9fc383dae0b2
Gabor Pali <pali.gabor@gmail.com>
2012-05-12 19:30:46 +0200

    devel/hs-reactive: Refine patch

--- devel/hs-reactive/files/patch-src__FRP__Reactive__Internal__Chan.hs
+++ devel/hs-reactive/files/patch-src__FRP__Reactive__Internal__Chan.hs
@@ -1,8 +1,8 @@
 --- ./src/FRP/Reactive/Internal/Chan.hs.orig	2010-07-28 18:48:55.000000000 +0200
-+++ ./src/FRP/Reactive/Internal/Chan.hs	2012-05-12 14:04:08.000000000 +0200
++++ ./src/FRP/Reactive/Internal/Chan.hs	2012-05-12 19:30:09.000000000 +0200
 @@ -1,4 +1,4 @@
 -{-# LANGUAGE CPP #-}
-+{-# LANGUAGE CPP StandaloneDeriving #-}
++{-# LANGUAGE CPP, StandaloneDeriving #-}
  {-# OPTIONS_GHC -Wall #-}
  -----------------------------------------------------------------------------
  -- |
