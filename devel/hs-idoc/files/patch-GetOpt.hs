1ae60f76a49479f5d680a16b158a0cce759d5259
Gabor Pali <pali.gabor@gmail.com>
2012-05-13 01:02:51 +0200

    devel/hs-idoc: Fix patches

--- devel/hs-idoc/files/patch-GetOpt.hs
+++ devel/hs-idoc/files/patch-GetOpt.hs
@@ -1,5 +1,5 @@
 --- ./GetOpt.hs.orig	2002-02-18 01:54:06.000000000 +0100
-+++ ./GetOpt.hs	2012-05-12 21:54:16.000000000 +0200
++++ ./GetOpt.hs	2012-05-13 00:56:23.000000000 +0200
 @@ -19,7 +19,7 @@
  
  module GetOpt (ArgOrder(..), OptDescr(..), ArgDescr(..), usageInfo, getOpt) where
