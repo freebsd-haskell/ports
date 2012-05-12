ca73db749a27ecccae8e4d66da5be50d3b0660c3
Gabor Pali <pali.gabor@gmail.com>
2012-05-12 23:52:01 +0200

    devel/hs-BNFC: Fix patch

--- devel/hs-BNFC/files/patch-formats__f-sharp__FSharpTop.hs
+++ devel/hs-BNFC/files/patch-formats__f-sharp__FSharpTop.hs
@@ -1,7 +1,11 @@
 --- ./formats/f-sharp/FSharpTop.hs.orig	2010-09-15 16:26:38.000000000 +0200
-+++ ./formats/f-sharp/FSharpTop.hs	2012-05-12 22:19:35.000000000 +0200
-@@ -38,7 +38,6 @@
- 
++++ ./formats/f-sharp/FSharpTop.hs	2012-05-12 23:50:50.000000000 +0200
+@@ -35,10 +35,9 @@
+ import CFtoXML
+ import GetCF
+ import Utils
+-
++import System.Exit
  import Char
  import Data.Maybe (fromMaybe,maybe)
 -import System
