6130f5a43facb830a0237fdbbaaf836f6607d06a
Gabor Pali <pali.gabor@gmail.com>
2012-05-12 14:04:55 +0200

    devel/hs-reactive: Fix build

---
+++ devel/hs-reactive/files/patch-src__FRP__Reactive__Internal__Chan.hs
@@ -0,0 +1,8 @@
+--- ./src/FRP/Reactive/Internal/Chan.hs.orig	2010-07-28 18:48:55.000000000 +0200
++++ ./src/FRP/Reactive/Internal/Chan.hs	2012-05-12 14:04:08.000000000 +0200
+@@ -1,4 +1,4 @@
+-{-# LANGUAGE CPP #-}
++{-# LANGUAGE CPP StandaloneDeriving #-}
+ {-# OPTIONS_GHC -Wall #-}
+ -----------------------------------------------------------------------------
+ -- |
