fcaa1b80e03726d9bc9320e5e0d243f86b075ba9
Gabor Pali <pali.gabor@gmail.com>
2012-07-30 19:19:37 +0100

    Remove compatibility fixes for pkgconf: 0.8.5 addresses these problems

--- lang/ghc/files/patch-libraries-Cabal-Cabal-Distribution-Simple-Program-Builtin.hs
+++
@@ -1,9 +0,0 @@
---- ./libraries/Cabal/Cabal/Distribution/Simple/Program/Builtin.hs.orig	2012-02-01 18:10:49.000000000 +0000
-+++ ./libraries/Cabal/Cabal/Distribution/Simple/Program/Builtin.hs	2012-07-29 20:13:00.000000000 +0100
-@@ -265,5 +265,5 @@
- 
- pkgConfigProgram :: Program
- pkgConfigProgram = (simpleProgram "pkg-config") {
--    programFindVersion = findProgramVersion "--version" id
-+    programFindVersion = findProgramVersion "--modversion pkg-config" id
-   }
