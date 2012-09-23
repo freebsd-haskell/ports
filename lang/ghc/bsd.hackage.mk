34c9a5bf193b6200e51b85ba567c7685abdd98b5
Gabor Pali <pali.gabor@gmail.com>
2012-09-23 10:56:30 +0200

    Add www/hs-activehs

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -13,6 +13,7 @@
 
 abstract-deque_port=		devel/hs-abstract-deque
 abstract-par_port=		devel/hs-abstract-par
+activehs_port=			www/hs-activehs
 activehs-base_port=		devel/hs-activehs-base
 aeson_port=			converters/hs-aeson
 Agda_port=			math/hs-Agda		# executable
