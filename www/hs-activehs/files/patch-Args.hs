d17c0a7cd73c0222c9e906a5d760875418d6d251
Gabor Pali <pali.gabor@gmail.com>
2012-10-05 09:56:28 +0200

    www/hs-activehs: Fix build and plist

--- www/hs-activehs/files/patch-Args.hs
+++ www/hs-activehs/files/patch-Args.hs
@@ -1,6 +1,6 @@
---- ./Args.hs.orig	2012-10-04 22:28:59.000000000 +0200
-+++ ./Args.hs	2012-10-04 22:29:07.000000000 +0200
-@@ -7,6 +7,8 @@
+--- ./Args.hs.orig	2012-02-21 16:25:44.000000000 +0100
++++ ./Args.hs	2012-10-05 00:31:05.000000000 +0200
+@@ -7,11 +7,14 @@
  import System.Console.CmdArgs.Implicit
  import System.FilePath
  
@@ -9,7 +9,21 @@
  ------------------
  
  data Args
-@@ -56,7 +58,7 @@
+     = Args
+         { sourcedir     :: String
++        , includedir    :: [String]
+         , gendir        :: String
+         , exercisedir   :: String
+         , templatedir   :: String
+@@ -36,6 +39,7 @@
+ myargs :: Args
+ myargs = Args
+         { sourcedir     = "."     &= typDir         &= help "Directory of lhs files to serve. Default is '.'"
++        , includedir    = []      &= typDir         &= help "Additional include directory (eg. path of Agda Standard Library). You can specify more than one. Empty by default."
+         , gendir        = "html"  &= typDir         &= help "Directory to put generated content to serve. Default is 'html'"
+         , exercisedir   = "exercise" &= typDir      &= help "Directory to put generated exercises to serve. Default is 'exercise'"
+         , templatedir   = ""      &= typDir         &= help "Directory of html template files for pandoc. Default points to the distribution's directory."
+@@ -54,7 +58,7 @@
          , verboseinterpreter = False                &= help "Verbose interpreter output in the browser"
          , recompilecmd  = "ghc -O" &= typ "COMMAND" &= help "Command to run before page generation. Default is 'ghc -O'."
          , magicname    = "a9xYf"  &= typ "VARNAME"  &= help "Magic variable name."
