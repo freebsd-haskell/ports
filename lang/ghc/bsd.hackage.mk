ec8ef79454e92aad363c6696fafb5d6945b384ec
Gabor Pali <pali.gabor@gmail.com>
2013-01-29 07:46:05 +0100

    Add devel/hs-profunctors

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -298,6 +298,7 @@ porte_port=			ports-mgmt/hs-porte	# executable
 primitive_port=			devel/hs-primitive
 printxosd_port=			x11/hs-printxosd	# executable
 probability_port=		math/hs-probability
+profunctors_port=		devel/hs-profunctors
 project-template_port=		devel/hs-project-template
 PSQueue_port=			devel/hs-PSQueue
 pureMD5_port=			security/hs-pureMD5
