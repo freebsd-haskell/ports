3689ad693d20aa500c56572bc3f1966b99137df1
Gabor Pali <pali.gabor@gmail.com>
2013-02-17 00:39:01 +0100

    devel/hs-vty: 4.7.2 --> 4.7.3

--- devel/hs-vty/files/patch-src__Graphics__Vty__Error.hs
+++
@@ -1,8 +0,0 @@
---- ./src/Graphics/Vty/Error.hs.orig	2013-01-27 21:37:11.000000000 +0100
-+++ ./src/Graphics/Vty/Error.hs	2013-02-05 02:29:30.000000000 +0100
-@@ -8,4 +8,4 @@
- --
- -- These have fully qualified names by default since, IMO, exception handling requires this.
- data VtyException
--    =  VtyFailure String -- | Uncategorized failure specific to vty.
-+    =  VtyFailure String -- ^ Uncategorized failure specific to vty.
