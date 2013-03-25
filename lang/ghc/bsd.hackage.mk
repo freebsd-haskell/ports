7194e31a4d62addc7d595658872aa4414dc6d53e
Gabor Pali <pali.gabor@gmail.com>
2013-03-25 19:46:42 +0100

    Add textproc/hs-regex-tdfa

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -277,6 +277,7 @@ regex-base_port=		textproc/hs-regex-base
 regex-compat_port=		textproc/hs-regex-compat
 regex-pcre-builtin_port=	textproc/hs-regex-pcre-builtin
 regex-posix_port=		textproc/hs-regex-posix
+regex-tdfa_port=		textproc/hs-regex-tdfa
 resource-pool_port=		devel/hs-resource-pool
 resourcet_port=			devel/hs-resourcet
 RSA_port=			security/hs-RSA
