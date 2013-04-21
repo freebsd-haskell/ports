829a0e77c5bc2a6cdb7af63d22dac5b2e783d596
Gabor Pali <pali.gabor@gmail.com>
2013-04-21 21:04:28 +0200

    devel/hs-git-annex: 4.20130405 --> 4.20130417

--- devel/hs-git-annex/files/patch-Build__DesktopFile.hs
+++ devel/hs-git-annex/files/patch-Build__DesktopFile.hs
@@ -1,5 +1,5 @@
---- ./Build/InstallDesktopFile.hs.orig	2012-10-17 03:24:57.000000000 +0200
-+++ ./Build/InstallDesktopFile.hs	2012-11-06 11:26:23.567300019 +0100
+--- ./Build/DesktopFile.hs.orig	2013-04-17 18:09:27.000000000 +0200
++++ ./Build/DesktopFile.hs	2013-04-21 20:37:33.000000000 +0200
 @@ -59,11 +59,15 @@
  
  writeFDODesktop :: FilePath -> IO ()
@@ -7,8 +7,8 @@
 -	datadir <- ifM systemwideInstall ( return systemDataDir, userDataDir )
 +	prefix <- catchMaybeIO (getEnv "PREFIX")
 +	let (sddir,scdir) = case prefix of
-+	                      Just p  -> (p </> "share", p </> "etc/xdg")
-+	                      Nothing -> (systemDataDir, systemConfigDir)
++			      Just p  -> (p </> "share", p </> "etc/xdg")
++			      Nothing -> (systemDataDir, systemConfigDir)
 +	datadir <- ifM systemwideInstall ( return sddir, userDataDir )
  	writeDesktopMenuFile (desktop command) 
  		=<< inDestDir (desktopMenuFilePath "git-annex" datadir)
