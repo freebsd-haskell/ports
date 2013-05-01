6eb860e10b901fd993f5d69f703c86a9a15fce48
Gabor Pali <pali.gabor@gmail.com>
2013-05-01 02:02:11 +0200

    graphics/hs-HGL: Fix import

--- graphics/hs-HGL/files/patch-Graphics__HGL__X11__Display.hs
+++ graphics/hs-HGL/files/patch-Graphics__HGL__X11__Display.hs
@@ -1,11 +1,11 @@
 --- ./Graphics/HGL/X11/Display.hs.orig	2009-11-03 15:50:11.000000000 +0100
-+++ ./Graphics/HGL/X11/Display.hs	2013-05-01 01:54:22.000000000 +0200
++++ ./Graphics/HGL/X11/Display.hs	2013-05-01 02:01:44.000000000 +0200
 @@ -14,12 +14,12 @@
  import Control.Monad (when)
  import Data.Maybe (isJust)
  import System.Environment (getEnv)
 -import System.IO.Error (try)
-+import System.IO.Error (tryIOError)
++import System.IO.Error (tryIOError, catchIOError)
  import System.IO.Unsafe (unsafePerformIO)
  
  getDisplayName :: IO String
