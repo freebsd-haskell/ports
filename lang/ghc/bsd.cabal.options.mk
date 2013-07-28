d47e0c7f98779c18f9e972c28d72b1763d88c743
Gabor Pali <pali.gabor@gmail.com>
2013-07-28 17:38:22 +0100

    bsd.cabal.options.mk: Add support for simplified translation of flags

--- lang/ghc/bsd.cabal.options.mk
+++ lang/ghc/bsd.cabal.options.mk
@@ -51,4 +51,73 @@ OPTIONS_DEFINE+=	HSCOLOUR
 IGNORE?=	options fail: bsd.cabal.mk already includes bsd.options.mk
 .endif
 
+OPTIONS_DEFINE+=	${FLAGS_DEFINE}
+OPTIONS_SINGLE+=	${FLAGS_SINGLE}
+OPTIONS_MULTI+=		${FLAGS_MULTI}
+OPTIONS_DEFAULT+=	${FLAGS_DEFAULT}
+
 .include <bsd.port.options.mk>
+
+ALL_FLAGS:=		${FLAGS_DEFINE} ${FLAGS_SINGLE} ${FLAGS_MULTI}
+COMPLETE_FLAGS=		${ALL_FLAGS}
+
+.for f in ${ALL_FLAGS}
+.  if defined(FLAGS_SINGLE_${f})
+COMPLETE_FLAGS+=	${FLAGS_SINGLE_${f}}
+OPTIONS_SINGLE_${f}=	${FLAGS_SINGLE_${f}}
+.  endif
+.  if defined(FLAGS_MULTI_${f})
+COMPLETE_FLAGS+=	${FLAGS_MULTI_${f}}
+OPTIONS_MULTI_${f}=	${FLAGS_MULTI_${f}}
+.  endif
+.endfor
+
+.for f in ${COMPLETE_FLAGS}
+.  if defined(${f}_FLAG_DEPENDS)
+.    for r in ${${f}_FLAG_DEPENDS}
+.      if !${PORT_OPTIONS:M${r}}
+_${f}_DEPENDS_FAIL=	yes
+.      endif
+.    endfor
+.  endif
+.  if ${PORT_OPTIONS:M${f}} && !defined(_${f}_DEPENDS_FAIL)
+.    if defined(${f}_FLAG_ENABLE)
+CONFIGURE_ARGS+=	--flags="${${f}_FLAG_ENABLE}"
+.    endif
+.    if defined(${f}_FLAG_CONFIGURE_ON)
+CONFIGURE_ARGS+=	${${f}_FLAG_CONFIGURE_ON}
+.    endif
+.    if defined(${f}_FLAG_CABAL)
+USE_CABAL+=		${${f}_FLAG_CABAL}
+.    endif
+.    if defined(${f}_FLAG_EXECUTABLE)
+EXECUTABLE+=		${${f}_FLAG_EXECUTABLE}
+.    endif
+.    if defined(${f}_FLAG_LIB)
+LIB_DEPENDS+=		${${f}_FLAG_LIB}
+.    endif
+.    if defined(${f}_FLAG_XORG)
+USE_XORG+=		${${f}_FLAG_XORG}
+.    endif
+.    if defined(${f}_DYNAMIC_BROKEN) && ${PORT_OPTIONS:MDYNAMIC}
+BROKEN=			${${f}_DYNAMIC_BROKEN}
+.    endif
+.  else
+.    if defined(${f}_FLAG_ENABLE)
+CONFIGURE_ARGS+=	--flags="-${${f}_FLAG_ENABLE}"
+.    endif
+.    if defined(${f}_FLAG_CONFIGURE_OFF)
+CONFIGURE_ARGS+=	${${f}_FLAG_CONFIGURE_OFF}
+.    endif
+.  endif
+.endfor
+
+.if defined(FLAGS_ENABLE)
+CONFIGURE_ARGS+=	--flags="${FLAGS_ENABLE}"
+.endif
+
+.if defined(FLAGS_DISABLE)
+.  for f in ${FLAGS_DISABLE}
+CONFIGURE_ARGS+=	--flags="-${f}"
+.  endfor
+.endif
