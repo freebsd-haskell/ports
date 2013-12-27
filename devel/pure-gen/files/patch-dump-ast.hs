d29027d6e834e21119ba9186d2c897e96607eaa1
Gabor Pali <pali.gabor@gmail.com>
2013-12-27 21:22:01 +0100

    devel/pure-gen: Sync with the Ports Collection

--- devel/pure-gen/files/patch-dump-ast.hs
+++
@@ -1,7 +0,0 @@
---- ./dump-ast.hs.orig	2011-09-09 10:13:26.000000000 +0100
-+++ ./dump-ast.hs	2013-08-29 00:59:30.000000000 +0100
-@@ -1,3 +1,4 @@
-+{-# LANGUAGE FlexibleInstances #-}
- -- Dump an AST from Language.C into a Pure term
- -- 
- -- Takes a single filename on the input, dumps the Pure term on stdout
