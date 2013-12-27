68d65d18720f93d63b5b31fb29aa10a4bc0c9efe
Gabor Pali <pali.gabor@gmail.com>
2013-12-27 01:53:58 +0100

    Add security/hs-cipher-des

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -73,6 +73,7 @@ charset_port=			devel/hs-charset
 checkers_port=			devel/hs-checkers
 cipher-aes_port=		security/hs-cipher-aes
 cipher-blowfish_port=		security/hs-cipher-blowfish
+cipher-des_port=		security/hs-cipher-des
 cipher-rc4_port=		security/hs-cipher-rc4
 citeproc-hs_port=		textproc/hs-citeproc-hs
 classy-prelude_port=		devel/hs-classy-prelude
