8f5619644449059147ff0fe20bfd3473e70138c5
Gabor Pali <pali.gabor@gmail.com>
2014-03-29 22:22:47 +0100

    lang/ghc-devel: 7.7.20140123 --> 7.8.0.20140228

--- lang/ghc/bsd.cabal.options.mk
+++ lang/ghc/bsd.cabal.options.mk
@@ -19,7 +19,7 @@ HADDOCK_CMD?=	${LOCALBASE}/bin/haddock
 HSCOLOUR_CMD?=	${LOCALBASE}/bin/HsColour
 
 .if defined(I_WANT_GHC_HEAD)
-GHC_VERSION?=	7.7.20140123
+GHC_VERSION?=	7.8.0.20140228
 GHC_PORT?=	lang/ghc-devel
 GHC_PACKAGE?=	ghc-devel
 .else
