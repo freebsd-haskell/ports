6c862c2e8610334c3c415c9ff8ffc97a0e0a4120
Masafumi Max NAKANE <max@FreeBSD.org>
2014-06-14 03:23:32 +0900

    Update textproc/hs-pandoc, 1.12.3.1 -> 1.12.4.2.

--- textproc/hs-pandoc/files/patch-Setup.hs
+++ textproc/hs-pandoc/files/patch-Setup.hs
@@ -1,15 +1,15 @@
---- ./Setup.hs.orig	2013-12-09 04:22:42.000000000 +0100
-+++ ./Setup.hs	2013-12-27 02:49:01.000000000 +0100
-@@ -9,7 +9,7 @@
+--- Setup.hs.orig	2014-05-15 07:07:54.000000000 +0900
++++ Setup.hs	2014-06-06 06:26:14.644756979 +0900
+@@ -26,7 +26,7 @@
  import Distribution.Simple.LocalBuildInfo
           (LocalBuildInfo(..), absoluteInstallDirs)
  import Distribution.Verbosity ( Verbosity, silent )
 -import Distribution.Simple.InstallDirs (mandir, CopyDest (NoCopyDest), toPathTemplate)
-+import Distribution.Simple.InstallDirs (prefix, CopyDest (NoCopyDest), toPathTemplate)
++import Distribution.Simple.InstallDirs (prefix, mandir, CopyDest (NoCopyDest), toPathTemplate)
  import Distribution.Simple.Utils (installOrdinaryFiles, info)
  import Distribution.Simple.Test (test)
  import Prelude hiding (catch)
-@@ -59,7 +59,7 @@
+@@ -76,7 +76,7 @@
  installManpages :: PackageDescription -> LocalBuildInfo
                  -> Verbosity -> CopyDest -> IO ()
  installManpages pkg lbi verbosity copy =
