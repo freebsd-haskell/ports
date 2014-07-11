0b9c556d1b1671cd62d1fca0053d14a96117f916
Gabor Pali <pali.gabor@gmail.com>
2014-07-11 10:08:40 +0200

    Remove devel/hs-haskeline

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -208,7 +208,6 @@ happy_port=			devel/hs-happy		# executable
 hashable_port=			devel/hs-hashable
 hashed-storage_port=		devel/hs-hashed-storage
 hashtables_port=		devel/hs-hashtables
-haskeline_port=			devel/hs-haskeline
 haskell-lexer_port=		devel/hs-haskell-lexer
 haskell-names_port=		devel/hs-haskell-names
 haskell-packages_port=		devel/hs-haskell-packages
