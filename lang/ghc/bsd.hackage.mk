54672b53c4da3eb43541bb4d6e6122ad64f278bb
Gabor Pali <pali.gabor@gmail.com>
2014-07-11 22:32:06 +0200

    Remove net/hs-network-conduit

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -317,7 +317,6 @@ mysql_port=			databases/hs-mysql
 mysql-simple_port=		databases/hs-mysql-simple
 nats_port=			math/hs-nats
 network_port=			net/hs-network
-network-conduit_port=		net/hs-network-conduit
 network-fancy_port=		net/hs-network-fancy
 network-info_port=		net/hs-network-info
 network-multicast_port=		net/hs-network-multicast
