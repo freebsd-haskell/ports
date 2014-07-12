a872824130d6647389b56c17a21dbb3a85873b58
Gabor Pali <pali.gabor@gmail.com>
2014-07-12 10:49:39 +0200

    Add devel/hs-asn1-encoding

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -26,6 +26,7 @@ ansi-wl-pprint_port=		devel/hs-ansi-wl-pprint
 appar_port=			textproc/hs-appar
 arrows_port=			devel/hs-arrows
 asn1-data_port=			devel/hs-asn1-data
+asn1-encoding_port=		devel/hs-asn1-encoding
 asn1-types_port=		devel/hs-asn1-types
 async_port=			devel/hs-async
 atomic-primops_port=		devel/hs-atomic-primops
