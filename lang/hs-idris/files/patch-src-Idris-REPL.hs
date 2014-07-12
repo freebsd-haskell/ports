de31465a6bd66f44be68928d54ec5c39e7a08426
Gabor Pali <pali.gabor@gmail.com>
2014-07-12 14:08:23 +0200

    lang/hs-idris: 0.9.10.1 --> 0.9.13.1

--- lang/hs-idris/files/patch-src-Idris-REPL.hs
+++ lang/hs-idris/files/patch-src-Idris-REPL.hs
@@ -1,6 +1,6 @@
---- src/Idris/REPL.hs.orig	2013-12-03 00:35:51.000000000 +0100
-+++ src/Idris/REPL.hs	2013-12-28 02:10:12.810459819 +0100
-@@ -1176,19 +1176,28 @@
+--- ./src/Idris/REPL.hs.orig	2014-06-09 21:56:28.000000000 +0200
++++ ./src/Idris/REPL.hs	2014-07-15 14:46:34.000000000 +0200
+@@ -1241,19 +1241,28 @@
           Nothing -> return ()
           Just expr -> execScript expr
  
@@ -10,13 +10,12 @@
 -                      unless exists $ iLOG ("Creating " ++ dir)
 -                      runIO $ createDirectoryIfMissing True (dir </> "repl"))
 -         (\e -> return ())
--
--       historyFile <- fmap (</> "repl" </> "history") getIdrisUserDataDir
 +       env <- runIO $ getEnvironment
 +       let wantData = case (lookup "IDRIS_NOUSERDATA" env) of
 +                        Just _ -> False
 +                        _      -> True
-+
+ 
+-       historyFile <- fmap (</> "repl" </> "history") getIdrisUserDataDir
 +       history <- if wantData
 +         then do
 +           -- Create Idris data dir + repl history and config dir
@@ -30,11 +29,11 @@
 +           return (Just historyFile)
 +         else return Nothing
  
-        when (runrepl && not idesl) $ initScript
-        stvar <- runIO $ newMVar ist
-        when (runrepl && not idesl) $ startServer ist stvar inputs
--       when (runrepl && not idesl) $ runInputT (replSettings (Just historyFile)) $ repl ist stvar inputs
-+       when (runrepl && not idesl) $ runInputT (replSettings history) $ repl ist stvar inputs
-        when (idesl) $ ideslaveStart ist inputs
+        when (runrepl && not idesl) $ do
+ --          clearOrigPats
+          startServer orig inputs
+-         runInputT (replSettings (Just historyFile)) $ repl orig inputs
++         runInputT (replSettings history) $ repl orig inputs
+        when (idesl) $ ideslaveStart orig inputs
         ok <- noErrors
         when (not ok) $ runIO (exitWith (ExitFailure 1))
