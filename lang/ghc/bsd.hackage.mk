34be22d23db697ca8a1f6124a656b54d1be1fd8a
Gabor Pali <pali.gabor@gmail.com>
2015-07-31 12:51:26 +0200

    Remove devel/hs-llvm-general-pure

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -321,7 +321,6 @@ lifted-async_port=                              devel/hs-lifted-async
 lifted-base_port=                               devel/hs-lifted-base
 List_port=                                      devel/hs-List
 ListLike_port=                                  devel/hs-ListLike
-llvm-general-pure_port=                         devel/hs-llvm-general-pure
 logict_port=                                    devel/hs-logict
 ltk_port=                                       devel/hs-ltk
 maccatcher_port=                                net/hs-maccatcher
