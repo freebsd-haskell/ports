9355b0c9fa675da38775c97458e9f34a9ce3afd3
Gabor Pali <pali.gabor@gmail.com>
2015-08-01 07:24:26 +0200

    Add www/hs-ghcjs-codemirror 0.0.1

--- lang/ghc/bsd.hackage.mk
+++ lang/ghc/bsd.hackage.mk
@@ -208,6 +208,7 @@ ghc-mtl_port=                                   devel/hs-ghc-mtl
 ghc-paths_port=                                 devel/hs-ghc-paths
 ghc-pkg-lib_port=                               devel/hs-ghc-pkg-lib
 ghc-syb-utils_port=                             devel/hs-ghc-syb-utils
+ghcjs-codemirror_port=                          www/hs-ghcjs-codemirror
 gio_port=                                       devel/hs-gio
 git-annex_port=                                 devel/hs-git-annex                              # executable
 gitit_port=                                     www/hs-gitit                                    # executable
