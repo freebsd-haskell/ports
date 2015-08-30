d7e1088d34718094af23266788c68f64a1425b8a
Gabor Pali <pali.gabor@gmail.com>
2015-08-30 19:18:50 +0200

    lang/ghc: Bring in changes from DragonFly
    Submitted by:	marino
    PR:			202566

--- lang/ghc/files/patch-libraries_Cabal_Cabal_Distribution_Simple_GHC.hs
+++ lang/ghc/files/patch-libraries_Cabal_Cabal_Distribution_Simple_GHC.hs
@@ -1,6 +1,6 @@
 --- libraries/Cabal/Cabal/Distribution/Simple/GHC.hs.orig	2015-04-01 00:24:45 UTC
 +++ libraries/Cabal/Cabal/Distribution/Simple/GHC.hs
-@@ -875,7 +875,7 @@ getRPaths lbi clbi | supportRPaths hostO
+@@ -875,10 +875,10 @@ getRPaths lbi clbi | supportRPaths hostO
      supportRPaths Linux       = True
      supportRPaths Windows     = False
      supportRPaths OSX         = True
@@ -8,4 +8,8 @@
 +    supportRPaths FreeBSD     = True
      supportRPaths OpenBSD     = False
      supportRPaths NetBSD      = False
-     supportRPaths DragonFly   = False
+-    supportRPaths DragonFly   = False
++    supportRPaths DragonFly   = True
+     supportRPaths Solaris     = False
+     supportRPaths AIX         = False
+     supportRPaths HPUX        = False
